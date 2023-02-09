4f157388d869d90a9896c51ca2b0286023545790
Fernando Angolar <fernandoangolar581@gmail.com>
2023-02-09 10:18:45 +0100

    segundo commit

---
+++ rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/exception/handler/CustomizedResponseEntityExceptionHamdler.java
@@ -0,0 +1,40 @@
+package br.com.erudio.exception.handler;
+
+import br.com.erudio.exception.ExceptionResponse;
+import br.com.erudio.exception.UnsupportdMathOperationException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.util.Date;
+
+@ControllerAdvice
+@RestController
+public class CustomizedResponseEntityExceptionHamdler extends ResponseEntityExceptionHandler {
+
+    // Metodo responsavel para tratar exception genericas -> Exception-Handler
+    @ExceptionHandler(Exception.class)
+    public final ResponseEntity<ExceptionResponse> handlerAllException(Exception ex, WebRequest request) {
+
+        var exceprionResponse = new ExceptionResponse(
+                new Date(),
+                ex.getMessage(),
+                request.getDescription(false));
+
+        return new ResponseEntity<>(exceprionResponse, HttpStatus.INTERNAL_SERVER_ERROR);  // exception do tipo 500
+    }
+
+    @ExceptionHandler(UnsupportdMathOperationException.class)
+    public final ResponseEntity<ExceptionResponse> handlerBadRequestException( Exception ex, WebRequest request ) {
+        var exceptionResponse = new ExceptionResponse(
+                new Date(),
+                ex.getMessage(),
+                request.getDescription(false));
+        return new ResponseEntity<>(exceptionResponse, HttpStatus.BAD_REQUEST);
+    }
+
+}
