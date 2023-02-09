ae232becb1d582a511c64f91112928dda1c50355
Fernando Angolar <fernandoangolar581@gmail.com>
2023-02-09 09:29:26 +0100

    primeiro commit

---
+++ rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/Greeting.java
@@ -0,0 +1,20 @@
+package br.com.erudio;
+
+public class Greeting {
+
+    private final long id;
+    private final String contant;
+
+    public Greeting(long id, String contant) {
+        this.id = id;
+        this.contant = contant;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getContant() {
+        return contant;
+    }
+}
